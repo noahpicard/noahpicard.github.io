1a823bc55b20b7ec2011448b7737e5b0b0fd55cf
hollycontin@gmail.com <noah_picard@brown.edu>
2015-08-26 23:30:43 -0700

    Added click to holblended color changing and title changing

--- js/control.js
+++ js/control.js
@@ -1,11 +1,67 @@
 $(document).ready(function() {
 	var block = $('#header');
-	var color = Math.floor(Math.random() * 360);
-	var colorStr = 'hsl(' + color + ',50%,50%)';
-	block.css('background-color', colorStr);
+	block.css('background-color', getRandomColor());
 	block.css('color', 'white');
-
+	setInterval(changeBlockColor, 7000);
+	setTimeout(function() {
+		setInterval(changeHeaderTitle, 7000);
+	}, 500);
 });
+
+function changeHeaderTitle() {
+	var title = $('#headerTitle');
+	var startStr = 'Let\'s create ';
+	var endStr = '.';
+	var midStr = tools.titleList[randomRange(0, tools.titleList.length)];
+	title.text(startStr + midStr + endStr);
+}
+
+
+tools = {};
+
+tools.titleList = [
+'something beautiful',
+'something inspiring',
+'something unique',
+'a game to teach graphing',
+'a game to learn about vectors',
+'an app to share your stuff',
+'an app to teach martial arts',
+'an app to learn to dance',
+'a web puzzle game for a team',
+'new ways to share your stuff',
+'opportunities to see the world',
+'cheap solar water purifiers',
+'mappings for satellite data',
+'a stunning website',
+'new mathematical proofs',
+'data-driven fashion',
+'procedurally generated music',
+'location based artist promotion',
+'an ecofriendly teacher app',
+'a real life cooking game',
+'disease tracking through facebook',
+'a website to spread happiness',
+'neural network fraud detection',
+'animal based topographic mapping',
+'a climate action game',
+'decentralized security systems'
+];
+
+function changeBlockColor() {
+	var block = $('#header');
+	block.css('background-color', getRandomColor());
+}
+
+function randomRange(low, high) {
+	return Math.floor(Math.random() * (high-low)) + low;
+}
+
+function getRandomColor() {
+	var color = randomRange(0, 360);
+	var colorStr = 'hsl(' + color + ',50%,50%)';
+	return colorStr;
+}
 
 
 
