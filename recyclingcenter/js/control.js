e654cc359bfd0bb40425b44c60c4c14c217f1649
hollycontin@gmail.com <noah_picard@brown.edu>
2016-04-23 17:52:59 -0400

    Added bottlebillwebside

---
+++ recyclingcenter/js/control.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$('.imageFrame').css('min-height', $(window).height()+"px");
+	$(".seeMore").click(function(caller) {
+		console.log(caller);
+    	var current = caller.currentTarget;
+    	var expand = $(current.parentNode).children('.expand');
+    	if (expand.css('max-height') == '0px') {
+    		var totalHeight = 20;
+    		expand.children().each(function(){
+    			totalHeight = totalHeight + $(this).outerHeight(true);
+			});
+    		expand.css('max-height',totalHeight+'px');
+    		$(current).text('Hide Courses');
+    	} else {
+    		expand.css('max-height','0px');
+    		$(current).text('See Courses');
+    	}
+    	
+	});
+});
+
+
